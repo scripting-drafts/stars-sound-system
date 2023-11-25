84cf66cb3c45e4d33f34c185fa9fae1b0f57019c
gerard <12766134+scripting-drafts@users.noreply.github.com>
2023-11-25 20:14:19 +0100

    upgrade on catalog parsing

--- scripts/parse_stcatalog.py
+++ scripts/parse_stcatalog.py
@@ -3,120 +3,97 @@ import csv
 import re
 from tqdm import tqdm
 
-file = open(r'..\data\catalog', mode="rb")
+def load():
+    file = open(r'..\data\catalog', mode="rb")
+
+    return file
 
 chunk_size = 197
 Bright_Star_Number = 4
-name = 10
-dur_id = 11
+name = 9
+dur_id = 10
 
 
-'''
-1-  4  I4     ---     HR       [1/9110]+ Harvard Revised Number
-                                    = 
-   5- 14  A10    ---     Name     Name, generally Bayer and/or Flamsteed name
-  15- 25  A11    ---     DM       Durchmusterung Identification (zone in
-                                    bytes 17-19)
-  26- 31  I6     ---     HD       [1/225300]? Henry Draper Catalog Number
-  32- 37  I6     ---     SAO      [1/258997]? SAO Catalog Number
-  38- 41  I4     ---     FK5      ? FK5 star Number
-  
-      42  A1     ---     IRflag   [I] I if infrared source
-      43  A1     ---   r_IRflag  *[ ':] Coded reference for infrared source
-      44  A1     ---    Multiple *[AWDIRS] Double or multiple-star code
-  45- 49  A5     ---     ADS      Aitken's Double Star Catalog (ADS) designation
-  50- 51  A2     ---     ADScomp  ADS number components
-  52- 60  A9     ---     VarID    Variable star identification
-  61- 62  I2     h       RAh1900  ?Hours RA, equinox B1900, epoch 1900.0 (1)
-  63- 64  I2     min     RAm1900  ?Minutes RA, equinox B1900, epoch 1900.0 (1)
-  65- 68  F4.1   s       RAs1900  ?Seconds RA, equinox B1900, epoch 1900.0 (1)
-      69  A1     ---     DE-1900  ?Sign Dec, equinox B1900, epoch 1900.0 (1)
-  70- 71  I2     deg     DEd1900  ?Degrees Dec, equinox B1900, epoch 1900.0 (1)
-  72- 73  I2     arcmin  DEm1900  ?Minutes Dec, equinox B1900, epoch 1900.0 (1)
-  74- 75  I2     arcsec  DEs1900  ?Seconds Dec, equinox B1900, epoch 1900.0 (1)
-  76- 77  I2     h       RAh      ?Hours RA, equinox J2000, epoch 2000.0 (1)
-  78- 79  I2     min     RAm      ?Minutes RA, equinox J2000, epoch 2000.0 (1)
-  80- 83  F4.1   s       RAs      ?Seconds RA, equinox J2000, epoch 2000.0 (1)
-      84  A1     ---     DE-      ?Sign Dec, equinox J2000, epoch 2000.0 (1)
-  85- 86  I2     deg     DEd      ?Degrees Dec, equinox J2000, epoch 2000.0 (1)
-  87- 88  I2     arcmin  DEm      ?Minutes Dec, equinox J2000, epoch 2000.0 (1)
-  89- 90  I2     arcsec  DEs      ?Seconds Dec, equinox J2000, epoch 2000.0 (1)
-  91- 96  F6.2   deg     GLON     ?Galactic longitude (1)
-  97-102  F6.2   deg     GLAT     ?Galactic latitude (1)
- 103-107  F5.2   mag     Vmag     ?Visual magnitude (1)
-     108  A1     ---   n_Vmag    *[ HR] Visual magnitude code
-     109  A1     ---   u_Vmag     [ :?] Uncertainty flag on V
- 110-114  F5.2   mag     B-V      ? B-V color in the UBV system
-     115  A1     ---   u_B-V      [ :?] Uncertainty flag on B-V
- 116-120  F5.2   mag     U-B      ? U-B color in the UBV system
-     121  A1     ---   u_U-B      [ :?] Uncertainty flag on U-B
- 122-126  F5.2   mag     R-I      ? R-I   in system specified by n_R-I
-     127  A1     ---   n_R-I      [CE:?D] Code for R-I system (Cousin, Eggen)
- 128-147  A20    ---     SpType   Spectral type
-     148  A1     ---   n_SpType   [evt] Spectral type code
- 149-154  F6.3 arcsec/yr pmRA    *?Annual proper motion in RA J2000, FK5 system
- 155-160  F6.3 arcsec/yr pmDE     ?Annual proper motion in Dec J2000, FK5 system
-     161  A1     ---   n_Parallax [D] D indicates a dynamical parallax,
-                                    otherwise a trigonometric parallax
- 162-166  F5.3   arcsec  Parallax ? Trigonometric parallax (unless n_Parallax)
- 167-170  I4     km/s    RadVel   ? Heliocentric Radial Velocity
- 171-174  A4     ---   n_RadVel  *[V?SB123O ] Radial velocity comments
- 175-176  A2     ---   l_RotVel   [<=> ] Rotational velocity limit characters
- 177-179  I3     km/s    RotVel   ? Rotational velocity, v sin i
-     180  A1     ---   u_RotVel   [ :v] uncertainty and variability flag on
-                                    RotVel
- 181-184  F4.1   mag     Dmag     ? Magnitude difference of double,
-                                    or brightest multiple
- 185-190  F6.1   arcsec  Sep      ? Separation of components in Dmag
-                                    if occultation binary.
- 191-194  A4     ---     MultID   Identifications of components in Dmag
- 195-196  I2     ---     MultCnt  ? Number of components assigned to a multiple
-     197  A1     ---     NoteFlag [*] a star indicates that there is a note
-                                    (see file notes)'''
+def get_info(file):
+    info = ''.encode('ascii')
 
-info = []
-
-while True:
-    chunk = file.read(197)
-    # name = file.read(14)
-    # hr_name = file.read(25)
-    # discard = file.read(197-25)
-    # info.extend([chunk, name, hr_name])
-    
-    info.append(chunk)
-    
-    # print(chunk)
-    
-
-    if not chunk:
-        break
-
-
-
-def label_data(info):
-    '''Decodes string lists, removes characters, finds valuable strings'''
-    Harvard_Revised_Number = None
-    last_Harvard_Revised_Number = None
-    for chunk in info[:32]:
-        chunk = chunk.decode('ascii')
-        chunk = chunk.strip()
-        chunk = re.sub('\n', '', chunk)
-        Harvard_Revised_Number = re.findall(r'^\d+', chunk)
-        print(chunk)
+    while True:
+        chunk = file.read(180)
+        info = info + chunk
         
-        if '' not in Harvard_Revised_Number and Harvard_Revised_Number[:1]:
-            Harvard_Revised_Number = int(''.join(Harvard_Revised_Number))
+        if not chunk:
+            info = info.decode('ascii')
+            info = [each for each in info.split('\n')]
             
-            if last_Harvard_Revised_Number is None:
-                print(f'\nHarvard_Revised_Number: {Harvard_Revised_Number}\n\n\n')
+            return info
 
-            elif Harvard_Revised_Number == last_Harvard_Revised_Number + 1:
-                print(f'\nHarvard_Revised_Number: {Harvard_Revised_Number}\n\n\n')
+
+def label_info(data):
+    data = [each.split() for each in data]
+    # print(data)
+    info = {}
+    info['hardvard_number'] = []
+    info['name'] = []
+    info['Durchmusterung ID'] = []
+    info['Henry Draper Catalog Number'] = []
+    info['SAO Catalog Number'] = []
+    info['FK5 Star Number'] = []
+    info['Infrared'] = []
+    info['Infrared_ref'] = []
+
+    
+    for chunk in data:
+        if chunk[:1]:
+            info['hardvard_number'].append(chunk[0])
+            info['name'].append(chunk[1])
+            info['Durchmusterung ID'].append(chunk[2])
+            info['Henry Draper Catalog Number'].append(chunk[3] if re.match(r'[1-225300]', chunk[3]) else '')
+            info['SAO Catalog Number'].append(chunk[4] if re.match(r'[1-258997]', chunk[3]) else '')
+            info['FK5 Star Number'].append(chunk[5] if re.match(r'\S+', chunk[5]) else '')
+            info['Infrared'].append(chunk[6] if re.match('I', chunk[6]) else '')
+            if info['Infrared'][-1] == chunk[6]:
+                info['Infrared_ref'].append(chunk[7] if re.match(r'[ \':]', chunk[7]) else '')
             
-            last_Harvard_Revised_Number = Harvard_Revised_Number
+
+    return info
+
+
+# def label_data(info):
+#     '''Decodes string lists, removes characters, finds valuable strings'''
+#     Harvard_Revised_Number = None
+#     last_Harvard_Revised_Number = None
+#     for chunk in info[:3]:
+#         # chunk = chunk.decode('ascii')
+#         chunk = chunk.strip()
+#         chunk = re.sub('\n', '', chunk)
+#         Harvard_Revised_Number = re.findall(r'^\d+', chunk)
+#         print(chunk)
+        
+#         if '' not in Harvard_Revised_Number and Harvard_Revised_Number[:1]:
+#             Harvard_Revised_Number = int(''.join(Harvard_Revised_Number))
+
+#             if last_Harvard_Revised_Number is None:
+#                 print(f'\nHarvard_Revised_Number: {Harvard_Revised_Number}\n\n\n')
+
+#             elif Harvard_Revised_Number == last_Harvard_Revised_Number + 1:
+#                 print(f'\nHarvard_Revised_Number: {Harvard_Revised_Number}\n\n\n')
+            
+#             last_Harvard_Revised_Number = Harvard_Revised_Number
+
+        # print('\n\n\n')
     
-    
-label_data(info)
+
+file = load()
+info = get_info(file)
+print(info)
+
+info = label_info(info)
+# print(len(info['hardvard_number']))
+# print(len(info['name']))
+# # info = decode_dic(info)
+# print(info['hardvard_number'][:3])
+# print(info['name'][:3])
+# label_data(info)
 
 
 # info = [x for x in info if x != '']
---
+++ scripts/yale_bsc_parser.py
@@ -0,0 +1,117 @@
+import sys
+import math
+
+try:
+    constellation = sys.argv[1]
+except IndexError:
+    print('usage:\n'
+          'python {:s} <constellation>\n'
+          'where <constellation> is the three-letter abbreviation for a'
+          ' constellation name (e.g. Ori, Lyr, UMa, ...)'.format(sys.argv[0]))
+    sys.exit(1)
+
+class Star():
+    """
+    A class representing a star, with name, magnitude, right ascension (ra),
+    declination (dec) and projected co-ordinates x,y calculated by a class
+    method.
+
+    """
+
+    def __init__(self, name, mag, ra, dec):
+        """
+        Initializes the star object with its name and magnitude, and position
+        as right ascension (ra) and declination (dec), both in radians.
+
+        """
+
+        self.name = name
+        self.mag = mag
+        self.ra = ra
+        self.dec = dec
+
+    def project_orthographic(self, ra0, dec0):
+        """
+        Calculates, stores and returns the projected co-ordinates (x, y) of
+        this star's position using an orthographic projection about the
+        angular position (ra0, dec0).
+
+        """
+
+        delta_ra = self.ra - ra0
+        self.x = math.cos(self.dec) * math.sin(delta_ra)
+        self.y = math.sin(self.dec) * math.cos(dec0)\
+             - math.cos(self.dec) * math.cos(delta_ra) * math.sin(dec0)
+        return self.x, self.y
+
+stars = []
+with open(r'..\data\bsc5.dat', 'r') as fi:
+    for line in fi.readlines():
+        # print(line[11:14])
+        # line = line.decode('utf-8')
+        if line[11:14] != constellation:
+            # We have no interest of stars which do not belong to constellation
+            continue
+        name = line[4:14]
+        try:
+            mag = float(line[102:107])
+        except ValueError:
+            # some stars do not have magnitudes: ignore these entries
+            continue
+        # Right ascension (hrs, mins, secs), equinox J2000, epoch 2000.0
+        ra_hrs, ra_min, ra_sec = [float(x) for x in (line[75:77],
+                                                     line[77:79], line[79:83])]
+        # Declination (hrs, mins, secs), equinox J2000, epoch 2000.0
+        dec_deg, dec_min, dec_sec = [float(x) for x in (line[83:86],
+                                                     line[86:88], line[88:90])]
+        # Convert both RA and declination to radians
+        ra = math.radians((ra_hrs + ra_min/60 + ra_sec/3600) * 15.)
+        # NB in the Southern Hemisphere be careful to subtract the minutes and
+        # seconds from the (negative) degrees.
+        sgn = math.copysign(1, dec_deg)
+        dec = math.radians(dec_deg + sgn * dec_min/60 + sgn * dec_sec/3600)
+
+        # Create a new Star object and add it to the list of stars
+        stars.append(Star(name, mag, ra, dec))
+
+n = len(stars)
+if n==0:
+    print('Constellation {:s} not found.'.format(constellation))
+    sys.exit(1)
+else:
+    print('Found {:d} stars in the constellation {:s}'.format(n,constellation))
+
+# Now calculate the projected co-ordinates of each star, (x,y), finding the
+# maximum and minimum values and hence the aspect ratio for our image.
+# The "centre" of the constellation in RA, dec:
+ra0 = sum([star.ra for star in stars]) / n
+dec0 = sum([star.dec for star in stars]) / n
+x, y = [None]*n, [None]*n
+for i, star in enumerate(stars):
+    # Orthographic projection (ra, dec) -> (x1, y1)
+    x[i], y[i] = star.project_orthographic(ra0, dec0)
+xmin, xmax, ymin, ymax = min(x), max(x), min(y), max(y)
+aspect_ratio = (xmax-xmin)/(ymax-ymin)
+
+# The stars will be output on a canvas of dimensions width x height, with
+# some extra padding around the outside of the "paintable" area.
+padding = 50
+height = 500
+width = int(height * aspect_ratio)
+
+# Write the SVG image file for the constellation
+with open('{:s}.svg'.format(constellation), 'w') as f:
+    print('<?xml version="1.0" encoding="utf-8"?>', file=f)
+    print('<svg xmlns="http://www.w3.org/2000/svg"', file=f)
+    print('     xmlns:xlink="http://www.w3.org/1999/xlink"', file=f)
+    print('     width="{:d}" height="{:d}" style="background: #000000">'
+            .format(width + 2*padding, height + 2*padding), file=f)
+    for star in stars:
+        rx = (star.x - xmin) / (xmax - xmin)
+        ry = (star.y - ymin) / (ymax - ymin)
+        cx = padding + (1-rx) * (width - 2*padding)
+        cy = padding + (1-ry) * (height - 2*padding)
+        print('<circle cx="{cx:.1f}" cy="{cy:.1f}" r="{r:.1f}"'
+              ' stroke="none" fill="#ffffff" name="{name:s}"/>'.format(
+              cx=cx, cy=cy, r=max(1,5-star.mag), name=star.name), file=f)
+    print('</svg>', file=f)
